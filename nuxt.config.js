6ad793b201c5e8d9aa60d3bab0ddda8bd2dd7416
avral <qweasd_96@mail.ru>
2018-10-22 20:45:21 +0300

    update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,11 +31,7 @@ module.exports = {
 
     link: [
       // TODO Logo here
-			///{ rel: "apple-touch-icon",  sizes: "180x180", href: "/apple-touch-icon.png"},
-			///{ rel: "manifest", href: "/site.webmanifest"},
-    	///{ rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5"},
-			///{ rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"},
-			///{ rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"},
+			{ rel: 'icon', type: 'image/x-icon', href: 'favicon.ico'},
 
       // TODO Выпилить бутстрап полностью и взять только утилиты и сетку
 			//{ rel: "stylesheet", type: "text/css", href: "https://cdn.jsdelivr.net/npm/bootstrap-utilities@4.0.0/bootstrap-utilities.css"},
