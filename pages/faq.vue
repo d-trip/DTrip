be5d110fb7a58c15801c2cf155dd4dff47d2e524
avral <qweasd_96@mail.ru>
2018-11-05 21:12:54 +0300

    create FAQ page

---
+++ pages/faq.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  .container
+    h1.text-center.mt-3.display-1 FAQ
+
+    h1 How to authorize?
+    p
+      | Just click to 
+      b login 
+      | button on top of site. And Follow the steemconnect  instructions.
+
+    h1 Where can I get an account?
+    //img(src="/logos/steem-logo.png" height="100")
+    p
+      | If you do not have a STEAM account, you can register new one on  
+      a(href="https://steemhunt.com/sign-up" target="_blank") SteemHunt 
+      | or 
+      a(href="https://signup.steemit.com/" target="_blank") Steemit
+
+
+    h1 How to write publication
+    p Log in, after that go to main page and click 
+      b Create a publication 
+      | button.
+</template>
+
+<script>
+export default {
+  layout: 'full-width',
+}
+</script>
